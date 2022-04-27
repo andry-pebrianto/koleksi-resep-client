babc64ff32b083f5d136e27d36cf1dc0c5ddc0e2
Andry Pebrianto <andrypeb227@gmail.com>
2022-04-28 02:56:51 +0700

    Menerapkan redux-persist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import "./assets/styles/utility.css";
-import React from 'react'
+import React from "react";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import Router from "./router";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Router />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
     </Provider>
-  )
+  );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,23 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducers from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
 
 const middleware = applyMiddleware(thunk);
 const store = createStore(
-  rootReducers,
+  persistedReducer,
   process.env.REACT_APP_NODE_ENV === "production"
     ? middleware
     : composeWithDevTools(middleware)
 );
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
