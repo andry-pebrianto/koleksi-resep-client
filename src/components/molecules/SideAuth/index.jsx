e9ea58899b380b75d87eab72a4536b306cdb52fa
Andry Pebrianto <andrypeb227@gmail.com>
2022-07-03 00:04:54 +0700

    Update logo mama recipe to koleksi resep

--- src/components/molecules/SideAuth/index.jsx
+++ src/components/molecules/SideAuth/index.jsx
@@ -6,8 +6,9 @@ function SideAuth() {
   return (
     <div className="side col-sm-5 col-md-6 d-none d-sm-flex">
       <div className="icon text-center w-100">
-        <Link to="/">
+        <Link className="text-decoration-none" to="/">
           <Logo width="100px" height="120px" color="#fff" />
+          <p className="p-0 m-0 text-white">Koleksi Resep</p>
         </Link>
       </div>
     </div>
