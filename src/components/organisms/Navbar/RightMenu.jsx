58ead41a7031cf6af7abfac159c75e7f7f1eb713
Andry Pebrianto <andrypeb227@gmail.com>
2023-04-15 10:02:14 +0700

    Integrasi fungsi logout dengan api agar menghapus refresh token di database

--- src/components/organisms/Navbar/RightMenu.jsx
+++ src/components/organisms/Navbar/RightMenu.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import Swal from 'sweetalert2';
-import { FaRegUser } from 'react-icons/fa';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import Swal from "sweetalert2";
+import { FaRegUser } from "react-icons/fa";
+import axios from "axios";
 
 function RightMenu({ isLoggedIn, transparent }) {
   const navigate = useNavigate();
 
   const logout = () => {
     Swal.fire({
-      title: 'Are you sure?',
-      text: 'You will be Logout!',
-      icon: 'warning',
+      title: "Are you sure?",
+      text: "You will be Logout!",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, Logout!',
-    }).then((result) => {
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Logout!",
+    }).then(async (result) => {
       if (result.isConfirmed) {
+        await axios.delete(
+          `${process.env.REACT_APP_API_URL}/auth/logout/${localStorage.getItem(
+            "refreshToken"
+          )}`
+        );
         localStorage.clear();
-        return navigate('/auth');
+        return navigate("/auth");
       }
 
       return 0;
@@ -43,7 +49,7 @@ function RightMenu({ isLoggedIn, transparent }) {
         <Link
           to="/auth"
           className={`nav-login ms-2 ${
-            transparent ? 'text-white' : 'color-blue'
+            transparent ? "text-white" : "color-blue"
           }`}
         >
           Login
